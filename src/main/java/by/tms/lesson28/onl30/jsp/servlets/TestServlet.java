66caa304697c94aa110f74b61e19ffa0f2705b97
alex garag <alex.garag@bk.ru>
2024-09-08 11:50:01 +0300

    сделал User в model

---
+++ src/main/java/by/tms/lesson28/onl30/jsp/servlets/TestServlet.java
@@ -0,0 +1,16 @@
+package by.tms.lesson28.onl30.jsp.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
